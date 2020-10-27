a27582f6f18f1df6ed0503d135adbb674fa65129
Eight8 <66164881+Eight8-7020@users.noreply.github.com>
2020-10-27 20:45:52 +0000

    Update ping.js

--- ping.js
+++ ping.js
@@ -2,7 +2,7 @@ module.exports = function({bot, commands}) {
         commands.addGlobalCommand ('ping', '',(msg, args) => {
 		bot.createMessage(msg.channel.id, ":ping_pong: Pinging...").then(m =>{
 			const ping = m.timestamp - msg.timestamp;
-			m.edit(":ping_pong: Pong! Your Ping Is: " + ping + "ms");
+			m.edit(`:ping_pong: Pong! Your ping is ${ping}ms`);
 		});
 	});
 };
